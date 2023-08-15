121c54821eb31ec6165b64ca7f10b414ca4dd5a0
Shane Castle <shane.castle@rci.rogers.ca>
2023-08-15 05:11:19 -0400

    updated motor control for motor test

--- motor_test_5_all_motors/motor_test_5_all_motors.ino
+++ motor_test_5_all_motors/motor_test_5_all_motors.ino
@@ -3,21 +3,29 @@
 
 #include <RPC.h>  // to boot the 2nd core
 
-// L298N Motor Driver 1 connections
-int driver1motor1enA = 4;  // Speed control for motor 1
-int driver1motor1in1 = 5;  // Direction control for Motor 1
-int driver1motor1in2 = 6;  // Direction control for Motor 1
-int driver1motor2enB = 9;
-int driver1motor2in3 = 11;
-int driver1motor2in4 = 10;
+//wiring:
+//           (PWM)               Encoder
+//           speed   direction   A    B
+// Motor 1 =   2     52,53       00   00
+// Motor 2 =   3     50,51       00   00
+// Motor 3 =   4     24,25       00   00
+// Motor 4 =   5     22,23       00   00
 
-// L298N Motor Driver 2 connections
-int driver2motor3enA = 56;  // Speed control for motor 3
-int driver2motor3in1 = 58;  // Direction control for Motor 3
-int driver2motor3in2 = 60;  // Direction control for Motor 3
-int driver2motor4enB = 62;
-int driver2motor4in3 = 64;
-int driver2motor4in4 = 66;
+// L298N Motor Driver speed  control
+int driver1motor1enA = 2;  // Speed control for motor 1
+int driver1motor2enB = 3;
+int driver2motor3enA = 4;  // Speed control for motor 3
+int driver2motor4enB = 5;
+
+// L298N Motor Driver 2 encoder
+int driver1motor1in1 = 52;  // Direction control for Motor 1
+int driver1motor1in2 = 53;  // Direction control for Motor 1
+int driver1motor2in3 = 50;
+int driver1motor2in4 = 51;
+int driver2motor3in1 = 24;  // Direction control for Motor 3
+int driver2motor3in2 = 25;  // Direction control for Motor 3
+int driver2motor4in3 = 22;
+int driver2motor4in4 = 23;
 
 
 #define M1_ENCODER_A 2  //  Motor 1 Encoder A
@@ -33,17 +41,17 @@ void setup() {
   // Serial.begin(9600);
   // Serial.write("Staring motor test");
 
-  // Set all the motor control pins to outputs
+  // Set motor speed control pins outputs
   pinMode(driver1motor1enA, OUTPUT);
   pinMode(driver1motor2enB, OUTPUT);
+  pinMode(driver2motor3enA, OUTPUT);
+  pinMode(driver2motor4enB, OUTPUT);
+
+  // Set the motor direction control
   pinMode(driver1motor1in1, OUTPUT);
   pinMode(driver1motor1in2, OUTPUT);
   pinMode(driver1motor2in3, OUTPUT);
   pinMode(driver1motor2in4, OUTPUT);
-
-
-  pinMode(driver2motor3enA, OUTPUT);
-  pinMode(driver2motor4enB, OUTPUT);
   pinMode(driver2motor3in1, OUTPUT);
   pinMode(driver2motor3in2, OUTPUT);
   pinMode(driver2motor4in3, OUTPUT);
@@ -55,8 +63,6 @@ void setup() {
   digitalWrite(driver1motor1in2, LOW);
   digitalWrite(driver1motor2in3, LOW);
   digitalWrite(driver1motor2in4, LOW);
-
-
   digitalWrite(driver2motor3in1, LOW);
   digitalWrite(driver2motor3in2, LOW);
   digitalWrite(driver2motor4in3, LOW);
@@ -64,21 +70,21 @@ void setup() {
 
 
   // set encoder pins
-  pinMode(M1_ENCODER_A, INPUT);
-  pinMode(M1_ENCODER_B, INPUT);
+//  pinMode(M1_ENCODER_A, INPUT);
+ // pinMode(M1_ENCODER_B, INPUT);
 
-  pinMode(M2_ENCODER_A, INPUT);
-  pinMode(M2_ENCODER_B, INPUT);
+//  pinMode(M2_ENCODER_A, INPUT);
+ // pinMode(M2_ENCODER_B, INPUT);
 }
 
 void loop() {
   directionControl();
   delay(1000);
-  speedControl();
+ // speedControl();
   delay(1000);
 
-  int a = digitalRead(M1_ENCODER_A);
-  int b = digitalRead(M1_ENCODER_B);
+ // int a = digitalRead(M1_ENCODER_A);
+ // int b = digitalRead(M1_ENCODER_B);
   // Serial.print(a * 5);
   //  Serial.print(" ");
   // Serial.print(b * 5);
@@ -90,15 +96,15 @@ void loop() {
 void directionControl() {
   // Set motors to maximum speed
   // For PWM maximum possible values are 0 to 255
-  analogWrite(driver1motor1enA, 255);
-  analogWrite(driver1motor2enB, 255);
+  analogWrite(driver1motor1enA, 200);
+  analogWrite(driver1motor2enB, 200);
 
   //// this crashes the giga
-  //  analogWrite(driver2motor3enA, 255);
-  //  analogWrite(driver2motor4enB, 255);
+  analogWrite(driver2motor3enA, 200);
+  analogWrite(driver2motor4enB, 200);
   // try digital instead
-  digitalWrite(driver2motor3enA, 255);
-  digitalWrite(driver2motor4enB, 255);
+ // digitalWrite(driver2motor3enA, 255);
+ // digitalWrite(driver2motor4enB, 255);
 
 
   // Turn on motor A & B
@@ -120,10 +126,10 @@ void directionControl() {
   digitalWrite(driver1motor2in3, LOW);
   digitalWrite(driver1motor2in4, HIGH);
 
-  //  digitalWrite(driver2motor3in1, LOW);
-  //  digitalWrite(driver2motor3in2, HIGH);
-  //  digitalWrite(driver2motor4in3, LOW);
-  //  digitalWrite(driver2motor4in4, HIGH);
+  digitalWrite(driver2motor3in1, LOW);
+  digitalWrite(driver2motor3in2, HIGH);
+  digitalWrite(driver2motor4in3, LOW);
+  digitalWrite(driver2motor4in4, HIGH);
 
 
   delay(2000);
@@ -134,10 +140,10 @@ void directionControl() {
   digitalWrite(driver1motor2in3, LOW);
   digitalWrite(driver1motor2in4, LOW);
 
-  //  digitalWrite(driver2motor3in1, LOW);
-  //  digitalWrite(driver2motor3in2, LOW);
-  //  digitalWrite(driver2motor4in3, LOW);
-  //  digitalWrite(driver2motor4in4, LOW);
+  digitalWrite(driver2motor3in1, LOW);
+  digitalWrite(driver2motor3in2, LOW);
+  digitalWrite(driver2motor4in3, LOW);
+  digitalWrite(driver2motor4in4, LOW);
 }
 
 // This function lets you control speed of the motors
